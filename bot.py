f56581846cb17cc1d842080925aaa7e4dcb3d593
vlad <162799637+carley252@users.noreply.github.com>
2025-10-24 13:19:49 +0300

    Migrate bot to telebot and add note management
    Refactor bot to use telebot library and implement note-taking features.

--- bot.py
+++ bot.py
@@ -1,24 +1,69 @@
-n = 5
+import telebot
 
-for i in range(1, n+1, -1):
-    spaces = ' ' * (i - 1)
-    stars = '*' * (n - i + 1 )
-    print(spaces+stars)
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+bot = telebot.TeleBot('BOT_TOKEN')
 
-TOKEN = "8354737608:AAH_0Q1OyTWwk8jJj4Wc4lGc4syH-JJMUTc"  # сюда вставь свой токен
+@bot.message_handler(commands=['start'])
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+def greeting(message):
+    bot.send_message(message.chat.id, "Привет! Я твой личный бот. Здесь можно вести заметки и получать напоминания. Чтобы узнать команды, напиши /help.")   
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text.lower() == "привет":
-        await message.reply("Привет! Я бот!")
-    elif message.text.lower() == "выход":
-        await message.reply("Пока!")
-        await bot.close()
+@bot.message_handler(commands=['help'])
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
+def bot_help(message):
+    bot.send_message(message.chat.id,"Вот что я умею:\n"
+"/start — запуск бота и приветствие\n"
+"/add — добавить новую заметку\n"
+"/list — показать все заметки\n"
+"/delete — удалить заметку по номеру\n"
+"/help — показать этот текст снова")
+notes = []
+@bot.message_handler(commands=['list'])
+
+def all_notes (message):
+    if notes:
+        bot.send_message(message.chat.id, f"Вот все твои заметки:\n" + "\n".join(notes)) 
+    else:
+        bot.send_message(message.chat.id, 'У тебя пока нет заметок!')
+
+@bot.message_handler(commands=['add'])
+
+def add_note(message):
+    bot.send_message(message.chat.id, "Напишите заметку: ")
+    bot.register_next_step_handler(message, save_note) 
+def save_note(message):
+    new_note = message.text
+    notes.append(new_note)
+    bot.send_message(message.chat.id, f"Заметка {new_note} успешно добавлена! 📝") 
+
+
+@bot.message_handler(commands=['delete'])
+
+def delete_note(message):
+    bot.send_message(message.chat.id, f"Выберите заметку которую хотите удалить:\n " + "\n".joinnotes)
+   
+    message_text = ""
+    for i, element in enumerate(notes, start=1):
+        message_text += f'{i}. {element}\n'
+    bot.send_message(message.chat.id, message_text)
+    
+    
+    bot.register_next_step_handler(message, deleted_note)
+
+    
+    
+def deleted_note(message):
+    note_for_deleting = message.text
+    if note_for_deleting.isdigit():
+        note_for_deleting = int(note_for_deleting)
+        index = note_for_deleting - 1
+        if 0 <= index < len(notes):
+            notes.pop(index)
+            bot.send_message(message.chat.id, "Заметка удалена ✅")
+        else:
+            bot.send_message(message.chat.id, "Такой заметки нет")         
+    else:
+        bot.send_message(message.chat.id, "Введите номер заметки!")
+        bot.register_next_step_handler(message, deleted_note)
+      
+        
+bot.polling(none_stop=True)
